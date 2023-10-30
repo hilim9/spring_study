1b8511e0db5437117f0d8737c0f972429bb13a81
hilim9 <dlagydls1212@naver.com>
2023-10-30 17:54:46 +0900

    day05

---
+++ day05/src/main/java/config/ControllerConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import controllers.HelloController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ControllerConfig {
+
+    @Bean
+    public HelloController helloController() {
+        return new HelloController();
+    }
+
+
+}
